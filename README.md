719a7773198ac87ed64357b244b2eb5e6b980f6c
kaiaiaiii <kaihrig2012@gmail.com>
2024-11-25 16:06:25 +0100

    Mehr kommentiert als gearbeitet, aber hab ja auch noch Uni

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 My First try on building a telescope and also develop some kind of software for it. Will take some time
 I need to learn C++, I need to transform all the signals to some readable output and I need to build the whole telescope
 
-The building of the telescope, including plans and calculations can be found in the (what a surprise) buildings folder
+The building of the telescope, including plans and calculations can be found in the (what a surprise) building folder
 
 Other stuff will follow.
 
--- SignalTransformation/FourierTransform.cpp
+++ SignalTransformation/FourierTransform.cpp
@@ -1,15 +1,46 @@
 #include <iostream>
 #include <cmath>
 //Dataset will first be given by me/downloaded somewhere (link will follow) and later on be implemented by me directly
-
-
+//need to fix this one, its just the algorithm
+/*
+void fft(n, f) {
+    if (int n == 1){
+        return f;
+    } else { 
+        g = fft(float n/2, ());
+        u = fft(float n/2, ());
+        for(float k= 0; k<n/2+1, k++)
+            c_k=g_k+u_k*exp(-2*pi*i*k/n)
+            c_k+n/2=g_k-u_k*exp(-2*pi*i*k/n)
+            
+    }
+}
+//Normal fourier Transform?
+*/
+int calculator(int x, int y){
+    int x, y;
+    int sum;
+    std::cout << "Type a number: ";
+    std::cin >> x;
+    std::cout << "Type another number: ";
+    std::cin >> y;
+    
+    return x+y;
+}
 int main() {
+    double t;
+    std::cout << "Type a Number";
+    std::cin>> t;
     for (double t = 0; t<20.01; t=t+0.2){
     //double t = 1.0;]
 
     double result = cos(t);
     std::cout <<  result << "\n";
     }
+}
+    //for (int )
+
+
     /*
     string str1("Hello")
     ifstream f("Dataset.txt");
@@ -29,6 +60,6 @@ int main() {
         
     f.close();
 
-*/
+
     return 0;
-}
+} */
