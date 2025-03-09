d889712565cbaddf22202284a189242c8fc03f9f
kaiaiaiii <kaihrig2012@gmail.com>
2025-03-09 18:19:09 +0000

    Dataset auslesen geht, fehtlt string to int

--- SignalTransformation/FourierTransform.cpp
+++ SignalTransformation/FourierTransform.cpp
@@ -6,32 +6,34 @@
 #include <iomanip>
 #include <fstream>
 #include <filesystem>
-#include <bits/stdc++.h>
+#include <string>
+#include <sstream>
 using namespace std;
 //Dataset will first be given by me/downloaded somewhere (link: https://www.kaggle.com/datasets/suraj520/rf-signal-data) and later on be implemented by me directly
 
-void ReadFromFile(){
-    string Text_from_file;
+void CSV_Split()
+{   // Daten von File lesen
+    string Text_from_file, Dataset, String_Split;
     ifstream DFT_File("Dataset.txt");
-    string Dataset;
-    vector<string> CSV_Vektor;
     // Daten in String schreiben
     while (getline (DFT_File, Text_from_file)){
         Dataset = Text_from_file;
         //cout <<Dataset;
-    };
-    /*
-    cout << Dataset;
-    stringstream ss(str);
- 
-    while (ss.good()) {
-        string substr;
-        getline(ss, substr, ',');
-        CSV_Vektor.push_back(substr);
     }
-    cout << CSV_Vektor;*/
     DFT_File.close();
+    //String nach Kommas separieren
+    stringstream obj_ss(Dataset);
+    //cout <<String_Split << "\n\n";
+    // while loop function to go through source string until it is finished.
+    while (getline(obj_ss, String_Split, ',')) {
+        cout << String_Split << endl;
+       // istringstream Dataset ('(' + Dataset + ')');
+        //complex <float> complex_number;
+        //Dataset >> complex_number;
+        //cout << complex_number << "\n \n";
+    }
 }
+
 int main()
 {   
    double Vektor_Length;
@@ -45,23 +47,22 @@ int main()
     cin >> Inputvektor[k];
     // std::cout << Inputvektor;
    }
-   
-   //DFT Berechnung
-   vector<double> LsgVektor(Vektor_Length, 0.0);
-   vector<double> HilfsVektor(Vektor_Length, 0.0);
-   double iteration_count=Vektor_Length;
-   for (int j = 0; j< Vektor_Length; j++)
-   {
-        for (int i = 1;i < iteration_count; i++)
-        {
-            HilfsVektor[i]=j*i/(Vektor_Length+1);
-            cout << "Hilfsvektor " << i+1 << ": " << HilfsVektor[i] << "\n";
-        }
-        // es fehlt noch imag in der Gleichung
-        LsgVektor[j]=exp(-HilfsVektor[j]*2*3.14)*Inputvektor[j]; //std::imag*3,14*-2
-        cout << "LSGVektor " << j+1 << ": " << LsgVektor[j] << "\n";
-   } 
-   ReadFromFile();
+    //DFT Berechnung
+    vector<double> LsgVektor(Vektor_Length, 0.0);
+    vector<double> HilfsVektor(Vektor_Length, 0.0);
+    double iteration_count=Vektor_Length;
+    for (int j = 0; j< Vektor_Length; j++)
+    {
+         for (int i = 1;i < iteration_count; i++)
+         {
+             HilfsVektor[i]=j*i/(Vektor_Length+1);
+             cout << "Hilfsvektor " << i+1 << ": " << HilfsVektor[i] << "\n";
+         }
+         // es fehlt noch imag in der Gleichung
+         LsgVektor[j]=exp(-HilfsVektor[j]*2*3.14)*Inputvektor[j]; //std::imag*3,14*-2
+         cout << "LSGVektor " << j+1 << ": " << LsgVektor[j] << "\n";
+    } 
+   CSV_Split();
     return 0;
 }
 
