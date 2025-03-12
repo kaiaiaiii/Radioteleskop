3b8d1a2441feafa1a3d98b9efebbc8b31dd4e35d
kaiaiaiii <kaihrig2012@gmail.com>
2025-03-12 13:46:59 +0000

    Versuche die DFT in Python zu implementieren

--- SignalTransformation/FourierTransform.cpp
+++ SignalTransformation/FourierTransform.cpp
@@ -12,21 +12,14 @@
 #include "ReadFromFile.h"
 using namespace std;
 //Dataset will first be given by me/downloaded somewhere (link: https://www.kaggle.com/datasets/suraj520/rf-signal-data) and later on be implemented by me directly
+//Discrete Fourier Transform
+// Es fehtl Werte aus einem Headerfile hierhin zu uebernehmen, bzw Werte einer Funktion
 int main()
 {   
    double Vektor_Length;
-   cout << "Length input: ";
-   cin >> Vektor_Length;
-   vector<double> Inputvektor(Vektor_Length, 0.0);
-    // Vektor input, erstmal Manuell
-   for (int k =0; k< Vektor_Length; k++)
-   {
-    cout << "Position " << k+1 << ": ";
-    cin >> Inputvektor[k];
-    // std::cout << Inputvektor;
-   }
+   CSV_Split();
+   cout << CSV_Split().Testinteger;
     //DFT Berechnung
-    typedef complex<double> i;
     vector<complex<double>> LsgVektor(Vektor_Length, 0.0);
     vector<complex<double>> HilfsVektor(Vektor_Length, 0.0);
     double iteration_count=Vektor_Length;
@@ -43,7 +36,6 @@ int main()
          LsgVektor[j]=exp(-HilfsVektor[j]*2.0*acos(-1))*Inputvektor[j]; //std::imag*3,14*-2
          cout << "LSGVektor " << j+1 << ": " << LsgVektor[j] << "\n";
     } 
-    CSV_Split();
     return 0;
 }
 
