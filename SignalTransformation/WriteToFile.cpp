e5e30177d4ef990c261ad0b3d7cc660eaaf3ad72
ihrig kai <kai@msspc27.mss.uni-erlangen.de>
2025-03-28 08:54:43 +0100

    Neue files hinzugefuegt

---
+++ SignalTransformation/WriteToFile.cpp
@@ -0,0 +1,14 @@
+#include <complex>
+using namespace std;
+string ComplexerString;
+void WritToFile(ComplexerString){
+    ComplexerString="TEST";
+    for (int i=0; i<10; i++){
+        cout << "Test";
+    }
+}
+
+int main(){
+    WriteToFile(Bier);
+    return 0;
+}
