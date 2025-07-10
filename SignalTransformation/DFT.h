5a1091f3e0ca38c5a366fdbf4d911642cb255a91
ihrig kai <kai@msspc49.mss.uni-erlangen.de>
2025-07-10 16:46:22 +0200

    Headerfiles neu

--- SignalTransformation/DFT.h
+++ SignalTransformation/DFT.h
@@ -4,21 +4,6 @@
 #include <cmath>
 using namespace std; 
 double AbsolutWert;
-void DFT(vector<complex<double>> InputVektor){
-    double Vektor_Length = InputVektor.size();
-    vector<complex<double>> ErgebnisVektor(Vektor_Length, 0);
-    vector<complex<double>> Hilfsergebnis(Vektor_Length, 0)
-    complex<double> a=-1;
-    complex<double> imag = sqrt(a);
-    for (double k =0; k<(Vektor_Length); k++)
-        {
-            for (double j =0; j<(Vektor_Length); j++)
-            {
-                ErgebnisVektor[k]+= (1/sqrt(Vektor_Length))*InputVektor[j]*exp(-2*M_PI*imag*j*(k)/(Vektor_Length));
-            }
-            
-            cout << ErgebnisVektor[k];
-            AbsolutWert = abs(ErgebnisVektor[K]);
-
-        }
-}
+class DiscreteFourierTransform;
+protected:
+    void DFT(vector<complex<double>> InputVektor);
