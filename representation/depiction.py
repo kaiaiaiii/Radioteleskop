6d1c93a168bee4931a93b64565f8b0471dbf3e36
Kaiaiaiii <kaihrig2012@gmail.com>
2025-10-26 14:29:22 +0100

    erster WSL commit

--- representation/depiction.py
+++ representation/depiction.py
@@ -1,5 +1,31 @@
-from txt import read
+import pyvista as pv
+#import numpy as np
+import os
+import glob
+import matplotlib.pyplot as plt
 
+filepath= 'C:/Privat/Radioteleskop/DFT.txt'
+Data = []
+frequency = []
+amplitude = []
+ctr =0 
 
-for x in range(0, 3):
-    print("We're on time %d" % (x))
+with open(filepath) as file:
+    print
+    for line in file:
+        ctr += 1
+        
+        
+print(ctr)
+    
+plt.figure(figsize=(10, 5))
+#plt.plot(time_steps, vel_center, label='Zentrum', marker='o', color='orange')
+#plt.plot(time_steps, surf_F, label='Surface Tension force', marker='o', color='blue')
+plt.plot(time_steps, vel_edge, label='velocity_magnitude', marker='o', color='grey')
+plt.xlabel("Zeitschritt")
+plt.ylabel("Geschwindigkeit [m/s]")
+plt.title("Partikelgeschwindigkeit über die Zeit (ID-basiert)")
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.show()
