ff70d4c5d7c7dd73e049315517989fbf2a0d0292
kaiaiaiii <kaihrig2012@gmail.com>
2026-01-19 21:27:40 +0100

    Transformation geht, auf zum input reader

--- SignalInput/ReadFromSDR.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "../SignalTransformation/WriteToFile.h"
-using namespace std;
-
-while (SignalLesen = true)
-    {
-        WriteToFile(Signal);
-    }
---
+++ SignalTransformation/ReadDataFromPi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <wiringPi.h>
+#include "ReadFromSDR.cpp"
+
+using namespace std;
+
+int main(){
+    wiringPiSetup();
+    pinMode(0, Output);
+    pinMode(1, Input);
+    while(1){
+        if(digitalRead(1) ==1 )
+        {
+            cout<< "Test";
+        }
+    }
+    return 0;
+}
