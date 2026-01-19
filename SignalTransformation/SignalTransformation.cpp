cbed7a6bdcbb1a4fbced9670121c60a21bf0c8b7
kaiaiaiii <kaihrig2012@gmail.com>
2026-01-19 21:06:19 +0100

    Ergebnis wird in File geschrieben. Umwandlung String to complex fehlt noch

--- SignalTransformation/SignalTransformation.cpp
+++ SignalTransformation/SignalTransformation.cpp
@@ -22,10 +22,12 @@ int main(){
     
     size_t Vektor_Length = DFT_Ergebnis.size();
     vector<double> Magnitudenvektor(Vektor_Length), Phasenvektor(Vektor_Length);
+
     for (int k = 0; k < Vektor_Length; k++){
         Magnitudenvektor[k] += sqrt(DFT_Ergebnis[k].imag()*DFT_Ergebnis[k].imag() + DFT_Ergebnis[k].real()*(DFT_Ergebnis[k].real()));
         Phasenvektor[k] += atan2(DFT_Ergebnis[k].imag(), DFT_Ergebnis[k].real());
     }
+    
     SignalTransformation::WriteToFile(DFT_Ergebnis,Magnitudenvektor,Phasenvektor );
     return 0;
 }
