f512f4afbe8957b840c37357d86acadf570f65b0
kaiaiaiii <kaihrig2012@gmail.com>
2026-01-25 21:39:17 +0100

    Makefile, build und merge fehlen

--- main.cpp
+++ main.cpp
@@ -8,12 +8,13 @@ using namespace std;
 int main(){
     while (true){
         vector<complex<double>> Versuchsvektor = SignalTransformation::DFT({1,2,3,4});
-        cout << Versuchsvektor[2];
+        cout << Versuchsvektor[1] << " " << Versuchsvektor[2] << endl ;
         //for(int i =0; i<1000000000000000000 ; i++){
         //cout << i << endl;
        //} 
     }
     return 0;
+
 }
 /*Pseudocode fuer meine Erinnerung und ideen
 while true:
