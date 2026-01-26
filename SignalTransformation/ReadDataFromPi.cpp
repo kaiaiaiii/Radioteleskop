e5511f3463bae6c5992f84df2ef4745443c62cae
kaiaiaiii <kaihrig2012@gmail.com>
2026-01-26 13:49:30 +0100

    ReadDataFromPi erweitert

--- SignalTransformation/ReadDataFromPi.cpp
+++ SignalTransformation/ReadDataFromPi.cpp
@@ -1,17 +1,37 @@
+#include <gpiod.h>
 #include <iostream>
-#include "../ExterneLibraries/wiringPi.h"
+#include <unistd.h>
 
 using namespace std;
 
-int ReadDataFromPi(){
-    wiringPiSetup();
-    pinMode(0, Output);
-    pinMode(1, Input);
-    while(1){
-        if(digitalRead(1) ==1 )
-        {
-            cout<< "Test";
-        }
+int main() {
+    //geklaut, muss noch jetzt angepasst werden
+    const char* chipname = "gpiochip0";
+    const unsigned int line_num = 17;
+
+    gpiod_chip* chip = gpiod_chip_open_by_name(chipname);
+    if (!chip) {
+        cerr << "GPIO chip konnte nicht geöffnet werden\n";
+        return 1;
     }
+
+    gpiod_line* line = gpiod_chip_get_line(chip, line_num);
+    if (!line) {
+        cerr << "GPIO Line nicht gefunden\n";
+        return 1;
+    }
+
+    if (gpiod_line_request_input(line, "rpi-zero2-input") < 0) {
+        cerr << "GPIO konnte nicht als Input gesetzt werden\n";
+        return 1;
+    }
+
+    while (true) {
+        int value = gpiod_line_get_value(line);
+        cout << "GPIO 17: " << value << endl;
+        sleep(1);
+    }
+
+    gpiod_chip_close(chip);
     return 0;
 }
--- make
+++ make
@@ -1,17 +1,19 @@
 CXX = g++
 CXXFLAGS = -std=c++17 -Wall -Wextra
 
-OBJS = main.o SignalTransformation/SignalTransformation.o 
+OBJS = main.o SignalTransformation/SignalTransformation.o SignalTransformation/ReadDataFromPi.o
 
-RT: $(OBJS)
+main: $(OBJS)
 	$(CXX) $(OBJS) -o main
 
 main.o: main.cpp
 	$(CXX) $(OBJS) -c main.cpp
 
-
 SignalTransformation/SignalTransformation.o: SignalTransformation/SignalTransformation.cpp
 	$(CXX) $(OBJS) -c SignalTransformation/SignalTransformation.cpp
+
+SignalTransformation/SignalTransformation.o: SignalTransformation/ReadDataFromPi.cpp
+	$(CXX) $(OBJS) -c SignalTransformation/ReadDataFromPi.cpp -lgpiod
 
 clean:
 	rm -f main $(OBJS)
